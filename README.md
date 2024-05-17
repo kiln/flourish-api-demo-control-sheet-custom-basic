4819b85a8d46c206a8f96e8bce0b42f8533224e9
larsvers <lars@canva.com>
2024-05-17 09:41:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Flourish simple customized control sheet demo
+
+Using the concept of the control sheet moves API chart configurations from code to a spreadsheet, allowing non-technical users to control and maintain API charts. The control sheet is a flexible concept, not a recipe, and there are multiple ways of implementing a control sheet.
+
+This demo covers a specific and simple implementation, exposing a small set of Flourish bindings and settings for Line, Bar, and Pie charts in a simple way.
+
+See [here](TODO) for a more feature-rich but slightly more complex example. See [here](TODO LINK) for a more generic and flexible, yet complex example allowing any chart to be built and changed.
+
+## Control sheet mechanics
+
+[`app.js`](app.js) reads in a data sheet from [`data/data-sheet-simple.csv`](data/data-sheet-simple.csv) and the control sheet from [`data/control-sheet-simple.csv`](data/control-sheet-simple.csv) to compose the application.
+
+**Each row** of the control sheet represents a chart being built, and **each column** configures the placement, or the Flourish settings of the chart.
+
+You can expose more **settings** by creating a control sheet column for each setting as well as adding additional setter functions run in `mutateOptions`. `mutateOptions` is just a wrapper function around individual functions that overwrite the base visualization settings with the settings given in the control sheet.
+
+`mutateOptions`, for example, runs `setTitles`, which just checks if the title/subtitle is in the `controlData` and mutates the respective settings in `state` accordingly:
+
+```js
+function setTitles(controlData, state) {
+  if (controlData.title) state.state.layout.title = controlData.title;
+  if (controlData.subtitle) state.state.layout.subtitle = controlData.subtitle;
+}
+```
+
+You can easily add your own state mutation functions or extend this concept to bindings or even data.
+
+## Control sheet columns
+
+The following documents each column of the given [control sheet](data/control-sheet-simple.csv).
+
+### Layout
+
+`container`: The CSS selector for each section as specified in `index.html`.
+
+### Base chart settings
+
+`base_chart`: A Flourish chart ID of a published Flourish chart. The base chart's information is pulled in to set the chart's template, version, base settings, bindings, and data. Control sheet exposed options can be changed by the control sheet user.
+
+`direct_chart`: A Flourish chart ID of a published Flourish chart that gets embedded directly rather than built by the Live API. Useful escape hatch if you want to show a chart as is without any control sheet or data configurations.
+
+> [!NOTE]
+> `app.js` gets the base chart configuration via the public endpoint `https://public.flourish.studio/visualisation/${<base chart ID>}/visualisation-object.json`. An alternative would be to use the Live API's `base_visualisation_id` property (see [here](https://developers.flourish.studio/api/replicate-visualization/#2-call-the-api-passing-in-base_visualisation_id)). In this case, the bindings and data would be in an array-of-arrays format and would need to be updated in or converted into an array-of-arrays format.
+>
+> By pulling from the public `/visualisation-object.json` endpoint, bindings and data of the base chart configuration as well as the separately JSON fetched data is consistently in an array-of-objects format.
+
+`chart_type`: This example only works with the [Line, Bar, Pie template](https://app.flourish.studio/@flourish/line-bar-pie/24#chart_type), which allows you to specify different types of charts.
+
+### Settings
+
+The following columns allow the user to set Flourish settings as specified in the Template Settings section of each template's API documentation, like [for example here](https://app.flourish.studio/@flourish/line-bar-pie/24#api-template-settings-header).
+
+`title`: Set the visualization title.
+
+`subtitle`: Set the visualization subtitle.
+
+`color_overrides`: Set any desired color overrides.
+
+Note that this will only work with templates that expose these settings. The code can be updated to include different settings for different templates.
---
+++ app.js
@@ -0,0 +1,155 @@
+/* API KEY */
+const API_KEY = "ADD YOUR API KEY HERE";
+
+/* Fetch helper */
+
+async function getBaseChartConfig(controlData) {
+  // Iterate over the rows and add base chart configurations
+  for (const row of controlData) {
+    // Only done for base and not direct charts (which will be embedded rather
+    // than API produced)
+    if (row.base_chart) {
+      try {
+        // Fetching the base charct configs. Note, this should be optimised if
+        // you're expecting repeating base chart id's
+        row.base_chart_config = await d3.json(
+          `https://public.flourish.studio/visualisation/${row.base_chart}/visualisation-object.json`
+        );
+      } catch (error) {
+        // Basic error handling
+        console.error(
+          `Failed to fetch data for base_chart ${row.base_chart}:`,
+          error
+        );
+        row.base_chart_config = null;
+      }
+    }
+  }
+  return controlData;
+}
+
+/* Live API configuration mutation function. Add more alongside control sheet variables. */
+
+function setChartType(controlData, state) {
+  if (controlData.chart_type) state.state.chart_type = controlData.chart_type;
+}
+
+function setTitles(controlData, state) {
+  if (controlData.title) state.state.layout.title = controlData.title;
+  if (controlData.subtitle) state.state.layout.subtitle = controlData.subtitle;
+}
+
+function setColorOverrides(controlData, state) {
+  if (controlData.color_overrides)
+    state.state.color.categorical_custom_palette = controlData.color_overrides;
+}
+
+function mutateOptions(controlData, state) {
+  setChartType(controlData, state);
+  setTitles(controlData, state);
+  setColorOverrides(controlData, state);
+}
+
+/* Chart build functions */
+
+function buildAPIChart(controlData, dataset) {
+  const { base_chart_config: baseChartConfig, ...controlSheetData } =
+    controlData;
+
+  // Setting the API chart base configurations.
+  const base = {
+    api_key: API_KEY,
+    template: baseChartConfig.template,
+    version: baseChartConfig.version,
+    container: controlSheetData.container,
+  };
+
+  // The settings (`state`) and the bindings are being taken straight from the
+  // base chart configuration.
+  const state = {
+    state: _.cloneDeep(baseChartConfig.state),
+  };
+
+  const bindings = {
+    bindings: _.cloneDeep(baseChartConfig.bindings),
+  };
+
+  // The data comes from the dataset
+  const data = {
+    data: {
+      data: dataset,
+    },
+  };
+
+  // Options changed by the control sheet are muted here
+  mutateOptions(controlData, state);
+
+  // Compose the final Live API options object
+  const apiOptions = { ...base, ...state, ...bindings, ...data };
+
+  // Build the chart.
+  const hasVisual = controlData.hasOwnProperty("visual");
+
+  // Built on initial load but updated on update
+  if (!hasVisual) {
+    // We add the visual to the options object to test if we can update next
+    // time we see this chart
+    controlData.visual = new Flourish.Live(apiOptions);
+  } else {
+    controlData.visual.update(apiOptions);
+  }
+}
+
+function buildDirectChart(controlData) {
+  const directChartContainer = d3.select(controlData.container);
+
+  directChartContainer.selectAll("*").remove();
+
+  directChartContainer
+    .append("iframe")
+    .attr(
+      "src",
+      `https://flo.uri.sh/visualisation/${controlData.direct_chart}/embed`
+    )
+    .attr("title", "Interactive or visual content")
+    .attr("class", "flourish-embed-iframe")
+    .attr("frameborder", "0")
+    .attr("scrolling", "no")
+    .attr(
+      "sandbox",
+      "allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation"
+    )
+    .style("width", "100%")
+    .style("height", "100%");
+}
+
+function buildVisual(chartControlData, dataset) {
+  // Check what type of chart to build. If the control sheet has a `base_chart`
+  // column set, the chart will be API produced. If a `direct_chart` column is
+  // set, the chart will be embedded.
+  if (chartControlData.base_chart) buildAPIChart(chartControlData, dataset);
+  if (chartControlData.direct_chart) buildDirectChart(chartControlData);
+  if (!chartControlData.base_chart && !chartControlData.direct_chart)
+    throw new Error(
+      "Please assign either a base_chart or a direct_chart visualization id"
+    );
+}
+
+async function main(controlData, dataset) {
+  // Add base chart configurations to each `controlData` row
+  await getBaseChartConfig(controlData);
+  for (const chart of controlData) buildVisual(chart, dataset);
+}
+
+/* Fetch data */
+
+const chartInfo = d3.csv(
+  `data/control-sheet-simple.csv?${Math.random()}`,
+  d3.autoType
+);
+const chartData = d3.csv(
+  `data/data-sheet-simple.csv?${Math.random()}`,
+  d3.autoType
+);
+
+Promise.all([chartInfo, chartData]).then((res) => main(res[0], res[1]));
